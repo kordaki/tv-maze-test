4842a4661c43cc0b8af41888e45af1675163d781
Pouriya Kordaki <pouriya.kordaki@justeattakeaway.com>
2022-11-22 23:48:37 +0100

    new video route with id parameter

---
+++ src/views/VideoView.vue
@@ -0,0 +1,26 @@
+<script setup>
+import { ref, watch } from "vue";
+import { useRoute } from "vue-router";
+const route = useRoute();
+const { id } = route.params;
+const videoId = ref(id);
+const showId = () => console.log(id);
+
+watch(
+  () => route.params.id,
+  async (newId) => {
+    console.log("Video Id is" + newId);
+    videoId.value = newId;
+  },
+  {
+    immediate: true,
+  }
+);
+</script>
+
+<template>
+  <h1>
+    The video id is <b>{{ videoId }}</b>
+  </h1>
+  <button @click="showId">Hello</button>
+</template>
